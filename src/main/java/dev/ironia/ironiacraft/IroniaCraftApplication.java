6fdf40c1e1fc74d6d72c8d3ec4e09b6873eaacc9
Ivan Pendziukh <ivan@admin.itotolink.net>
2026-05-26 17:53:21 +0200

    Initial commit: IroniaCraft project setup

---
+++ src/main/java/dev/ironia/ironiacraft/IroniaCraftApplication.java
@@ -0,0 +1,13 @@
+package dev.ironia.ironiacraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroniaCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IroniaCraftApplication.class, args);
+    }
+
+}
